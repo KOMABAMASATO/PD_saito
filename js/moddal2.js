b96070ba372142a201be5671cdf48f11cd156fc6
KOMABA MASATO <153893569+KOMABAMASATO@users.noreply.github.com>
2024-01-09 15:52:53 +0900

    Add files via upload

---
+++ js/moddal2.js
@@ -0,0 +1,9 @@
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script>
+  $(document).ready(function () {
+    // Show the modal on page load
+    $(".announcement-section").show();
+
+    // You can add additional logic or effects if needed
+  });
+</script>
